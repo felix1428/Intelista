2adb66282520a15b78308e5bdcb3d04c23c77846
Your Name <you@example.com>
2024-08-22 20:16:21 +0530

    newerr

--- src/components/CardNonTech.js
+++ src/components/CardNonTech.js
@@ -155,7 +155,7 @@ export default function CardNonTech() {
                <button
                   type="button"
                   className="undo-button"
-                  onClick={() => navigate("/tech")}
+                  onClick={() => navigate("/nontech")}
                   >
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z"/><path d="M13.293 7.293 8.586 12l4.707 4.707 1.414-1.414L11.414 12l3.293-3.293-1.414-1.414z"/></svg>
                   </button>
--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import treasurehunt from '../images/treasurehunt.gif'
 import { useNavigate } from "react-router-dom";
+import paperpresent from '../images/paper-present.mp4';
+import programming from '../images/programming.mp4';
+import webdev from '../images/webdev.mp4';
+import technicalquiz from '../images/technicalquiz.mp4';
 
 export default function Tech() {
     const navigate = useNavigate();
@@ -13,9 +16,17 @@ export default function Tech() {
     };
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6 ml-9">
             <div className="max-w-xs rounded-md shadow-md bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800">
-                <img src={treasurehunt} alt="Event 1" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500 dark:bg-gray-500" />
+                <video 
+                    src={paperpresent} 
+                    alt="Event 1" 
+                    className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500 dark:bg-gray-500" 
+                    controls 
+                    autoPlay 
+                    muted 
+                    loop
+                />
                 <div className="flex flex-col justify-between px-16 py-6 space-y-8">
                     <div className="space-y-2">
                         <h2 className="text-3xl font-semibold tracking-wide">IDEA BLITZ</h2>
@@ -31,7 +42,15 @@ export default function Tech() {
             </div>
 
             <div className="max-w-xs rounded-md shadow-md bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800">
-                <img src="https://source.unsplash.com/random/300x300/?2" alt="Event 2" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500 dark:bg-gray-500" />
+                <video 
+                    src={programming} 
+                    alt="Event 2" 
+                    className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500 dark:bg-gray-500" 
+                    controls 
+                    autoPlay 
+                    muted 
+                    loop
+                />
                 <div className="flex flex-col justify-between px-16 py-6 space-y-8">
                     <div className="space-y-2">
                         <h2 className="text-3xl font-semibold tracking-wide">CODING SENSEI</h2>
@@ -47,7 +66,15 @@ export default function Tech() {
             </div>
 
             <div className="max-w-xs rounded-md shadow-md bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800">
-                <img src="https://source.unsplash.com/random/300x300/?3" alt="Event 3" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500 dark:bg-gray-500" />
+                <video 
+                    src={webdev} 
+                    alt="Event 3" 
+                    className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500 dark:bg-gray-500" 
+                    controls 
+                    autoPlay 
+                    muted 
+                    loop
+                />
                 <div className="flex flex-col justify-between px-16 py-6 space-y-8">
                     <div className="space-y-2">
                         <h2 className="text-3xl font-semibold tracking-wide">WEB DESIGN</h2>
@@ -63,7 +90,15 @@ export default function Tech() {
             </div>
 
             <div className="max-w-xs rounded-md shadow-md bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800">
-                <img src="https://source.unsplash.com/random/300x300/?4" alt="Event 4" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500 dark:bg-gray-500" />
+                <video 
+                    src={technicalquiz} 
+                    alt="Event 4" 
+                    className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500 dark:bg-gray-500" 
+                    controls 
+                    autoPlay 
+                    muted 
+                    loop
+                />
                 <div className="flex flex-col justify-between px-16 py-6 space-y-8">
                     <div className="space-y-2">
                         <h2 className="text-3xl font-semibold tracking-wide">TECHNICAL QUIZ</h2>
